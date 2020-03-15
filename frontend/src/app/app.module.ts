29e5253b20876190f1ccfaac90af7ec296da4763
Johan Gouws <johangouws16@gmail.com>
2020-03-15 17:12:28 +0200

    Added firebase to host project

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -9,10 +9,12 @@ import { MatProgressBarModule } from '@angular/material/progress-bar';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CardComponent } from './card/card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CardComponent
   ],
   imports: [
     BrowserModule,
